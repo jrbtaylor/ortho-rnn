4a40205ec70189024436370da5b5252d5c705504
Jason R B Taylor <jasonrbtaylor@gmail.com>
2016-09-20 15:47:15 -0400

    remove orthogonality penalties that didn't work (orthopen 1&2)

--- train.py
+++ train.py
@@ -20,9 +20,9 @@ import graph
 
 
 def experiment(learning_rate=1e-1, lr_decay=0.99, bptt_limit=784, momentum=0.9,
-               l1_reg=0, l2_reg=1e-3, n_epochs=500, init_patience=500, 
-               batch_size=1000, repeated_exp=10,
-               to_run=['gradclip','ortho1','ortho2','ortho3']):
+               l1_reg=0, l2_reg=1e-3, n_epochs=500, init_patience=20, 
+               batch_size=1000, repeated_exp=5,
+               to_run=['gradclip','ortho3','both']):
     
     # Load the data
     datasets = data.load('mnist.pkl.gz')
@@ -53,34 +53,6 @@ def experiment(learning_rate=1e-1, lr_decay=0.99, bptt_limit=784, momentum=0.9,
         model = recurrent.rnn(x,n_in,n_hidden,10,bptt_limit,rng)
         unclipped_cost = model.crossentropy(y)+l1_reg*model.L1+l2_reg*model.L2
         cost = theano.gradient.grad_clip(unclipped_cost,-clip_limit,clip_limit)
-        return optim.sgd(dataprep(n_in),model,cost,x,y,n_train_batches,
-                         n_valid_batches,n_test_batches,batch_size,
-                         learning_rate,lr_decay,momentum,
-                         init_patience,n_epochs)
-    
-    def test_orthopen(eps_idem,eps_norm,n_in,n_hidden,seed=1):
-        # Build the model
-        print('... building the orthogonality-constrained RNN')
-        x = T.tensor3('x')
-        y = T.ivector('y') # labels are a 1D vector of integers
-        rng = numpy.random.RandomState(seed)
-        model = recurrent.rnn_ortho(x,n_in,n_hidden,10,bptt_limit,rng)
-        cost = model.crossentropy(y)+l1_reg*model.L1+l2_reg*model.L2+ \
-               eps_idem*model.idem+eps_norm*model.norm
-        return optim.sgd(dataprep(n_in),model,cost,x,y,n_train_batches,
-                         n_valid_batches,n_test_batches,batch_size,
-                         learning_rate,lr_decay,momentum,
-                         init_patience,n_epochs)
-    
-    def test_orthopen2(eps_idem,eps_norm,n_in,n_hidden,seed=1):
-        # Build the model
-        print('... building the 2nd orthogonality-constrained RNN')
-        x = T.tensor3('x')
-        y = T.ivector('y') # labels are a 1D vector of integers
-        rng = numpy.random.RandomState(seed)
-        model = recurrent.rnn_ortho2(x,n_in,n_hidden,10,bptt_limit,rng)
-        cost = model.crossentropy(y)+l1_reg*model.L1+l2_reg*model.L2+ \
-               eps_idem*model.idem+eps_norm*model.norm
         return optim.sgd(dataprep(n_in),model,cost,x,y,n_train_batches,
                          n_valid_batches,n_test_batches,batch_size,
                          learning_rate,lr_decay,momentum,
@@ -139,9 +111,9 @@ def experiment(learning_rate=1e-1, lr_decay=0.99, bptt_limit=784, momentum=0.9,
         writer = csv.writer(file)
         if line==0:
             writer.writerow(('Hyperparameters','n_in','n_hidden',
-                             'Training loss','Training error%',
-                             'Validation loss','Validation error%',
-                             'Test error%',
+                             'Training loss','Training error',
+                             'Validation loss','Validation error',
+                             'Test error',
                              '|Wx|','|Wh|','|Wy|','|WWt-I|'))
         writer.writerow((hyperparam,n_in,n_hidden,
                          trn_loss,trn_error,
@@ -164,36 +136,6 @@ def experiment(learning_rate=1e-1, lr_decay=0.99, bptt_limit=784, momentum=0.9,
             log_results('gradclip.csv',idx,clip_limit,n_in,n_hidden,
                         trn,vld,tst,mntrs)
         graph.make_all('gradclip.csv')
-        
-    # hyperparameter search for orthogonality penalty
-    if any('ortho1' in s for s in to_run):
-        eps_idems = [10**x for x in range(-4,0)]
-        eps_norms = [10**x for x in range(-4,0)]
-        for idx,(eps_idem,eps_norm,n_in,n_hidden) in \
-                enumerate(itertools.product(eps_idems,
-                                            eps_norms,
-                                            n_ins,
-                                            n_hiddens)):
-            fn = lambda seed: test_orthopen(eps_idem,eps_norm,n_in,n_hidden,seed)
-            trn,vld,tst,mntrs = repeat_test(fn,repeated_exp)
-            log_results('orthopen1.csv',idx,[eps_idem,eps_norm],n_in,n_hidden,
-                        trn,vld,tst,mntrs)
-        graph.make_all('orthopen1.csv')
-        
-    # hyperparameter search for orthogonality penalty
-    if any('ortho2' in s for s in to_run):
-        eps_idems = [10**x for x in range(-4,0)]
-        eps_norms = [10**x for x in range(-4,0)]
-        for idx,(eps_idem,eps_norm,n_in,n_hidden) in \
-                enumerate(itertools.product(eps_idems,
-                                            eps_norms,
-                                            n_ins,
-                                            n_hiddens)):
-            fn = lambda seed: test_orthopen2(eps_idem,eps_norm,n_in,n_hidden,seed)
-            trn,vld,tst,mntrs = repeat_test(fn,repeated_exp)
-            log_results('orthopen2.csv',idx,[eps_idem,eps_norm],n_in,n_hidden,
-                        trn,vld,tst,mntrs)
-        graph.make_all('orthopen2.csv')
     
     # hyperparameter search for orthogonality penalty
     if any('ortho3' in s for s in to_run):
